fba018a54108f794352f17be67f7fa82c664cb4b
Bret Comnes <bcomnes@gmail.com>
2019-08-21 10:46:17 +0200

    standard --fix

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const podcastCategories = require('./')
 
 tape('export works', t => {
   t.ok(podcastCategories, 'podcast categories exported')
-  t.ok(podcastCategories['Society & Culture']['History'], 'podcast subcategories exported')
+  t.ok(podcastCategories['Society & Culture'].History, 'podcast subcategories exported')
 
   t.end()
 })
